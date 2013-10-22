9bea9b6f8783c26e825d69a1a9d26cb7a4b6a106
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2013-10-22 11:20:40 +0700

    Initial commit

---
+++ lib/Pod/Weaver/Section/Homepage/DefaultCPAN.pm
@@ -0,0 +1,60 @@
+package Pod::Weaver::Section::Homepage::DefaultCPAN;
+
+use 5.010001;
+use Moose;
+use Text::Wrap ();
+with 'Pod::Weaver::Role::Section';
+
+#use Log::Any '$log';
+
+use Moose::Autobox;
+
+# VERSION
+
+sub weave_section {
+  my ($self, $document, $input) = @_;
+
+  my $name = $input->{zilla}->name;
+  my $homepage = $input->{distmeta}{resources}{homepage} ?
+      $input->{distmeta}{resources}{homepage}{web} :
+          "https://metacpan.org/release/$name";
+
+  my $text = "Project homepage is at $homepage.";
+
+  $text = Text::Wrap::wrap(q{}, q{}, $text);
+
+  $document->children->push(
+    Pod::Elemental::Element::Nested->new({
+      command  => 'head1',
+      content  => 'HOMEPAGE',
+      children => [
+        Pod::Elemental::Element::Pod5::Ordinary->new({ content => $text }),
+      ],
+    }),
+  );
+}
+
+no Moose;
+1;
+# ABSTRACT: Add a HOMEPAGE section
+
+=for Pod::Coverage weave_section
+
+=head1 SYNOPSIS
+
+In your C<weaver.ini>:
+
+ [Homepage::DefaultCPAN]
+
+To specify a bugtracker other than C<https://metacpan.org/release/NAME>, in
+dist.ini:
+
+ [MetaResources]
+ homepage=http://example.com/
+
+
+=head1 DESCRIPTION
+
+This section plugin adds a HOMEPAGE section.
+
+=cut
